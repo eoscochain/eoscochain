3c2fce6a4587cedfec300a95479fddd2f0804512
RideWindX <ridewindx@163.com>
2018-08-20 15:26:09 +0800

    Fix docker build.sh

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t cochain/eoscochain:release -f Dockerfile.custom --build-arg HTTP_PROXY=http://localhost:1080 --build-arg HTTPS_PROXY=http://localhost:1080 --network=host $(dirname "${BASH_SOURCE[0]}")
+docker build -t cochain/eoscochain -f Dockerfile.custom --build-arg HTTP_PROXY=http://localhost:1080 --build-arg HTTPS_PROXY=http://localhost:1080 --network=host $(dirname "${BASH_SOURCE[0]}")
