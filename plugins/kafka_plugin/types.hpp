8cb69e54cc082908a49a5fabc59425acbdcb4ca0
RideWindX <ridewindx@163.com>
2018-09-01 15:31:47 +0800

    Remove local queue for kafka_plugin

--- plugins/kafka_plugin/types.hpp
+++ plugins/kafka_plugin/types.hpp
@@ -83,7 +83,7 @@ using ActionPtr = std::shared_ptr<Action>;
 
 FC_REFLECT_ENUM(kafka::TransactionStatus, (executed)(soft_fail)(hard_fail)(delayed)(expired)(unknown))
 
-FC_REFLECT(kafka::Block, (id)(num)(timestamp)(block)(tx_count)(action_count)(context_free_action_count))
+FC_REFLECT(kafka::Block, (id)(num)(timestamp)(lib)(block)(tx_count)(action_count)(context_free_action_count))
 FC_REFLECT(kafka::Transaction, (id)(block_id)(block_num)(block_time)(block_seq)(action_count)(context_free_action_count))
 FC_REFLECT(kafka::TransactionTrace, (id)(scheduled)(status)(net_usage_words)(cpu_usage_us)(exception))
 FC_REFLECT(kafka::Action, (global_seq)(recv_seq)(parent_seq)(account)(name)(auth)(data)(receiver)(auth_seq)(code_seq)(abi_seq)(tx_id)(console))
